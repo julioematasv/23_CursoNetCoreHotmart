38ec07e00597127511cb490b24dcd701b53179b3
Julio Mata <julioematasv@gmail.com>
2023-03-27 12:02:29 +0200

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoNetoreHotmart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int year = 2023;
+            double decimales = 2.5;
+            string nombre = "Julio";
+            Console.WriteLine("Hola Julio, en Hotmart estamos en el año: " + year);
+            Console.WriteLine("Otra forma de mostrar datos: es el año {0} y el decimal {1}", year,decimales);
+            Console.WriteLine("Hola {1}, en Hotmart estamos en el año: {0}",year,nombre);
+
+            Console.ReadLine();
+        }
+    }
+}
