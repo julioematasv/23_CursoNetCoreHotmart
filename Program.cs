fffeb7f6d84801d3cbfd3e800d8a3423a9f2c8e0
Julio Mata <julioematasv@gmail.com>
2023-03-27 12:32:57 +0200

    Video sobre operadores aritmeticos

--- Program.cs
+++ Program.cs
@@ -10,6 +10,11 @@ namespace CursoNetoreHotmart
     {
         static void Main(string[] args)
         {
+            int valor1 = 3;
+            int valor2 = 6;
+            var suma = valor1 + valor2;
+            Console.WriteLine("El valor de la suma es: {0}",suma);
+
             int year = 2023;
             double decimales = 2.5;
             string nombre = "Julio";
@@ -17,7 +22,21 @@ namespace CursoNetoreHotmart
             Console.WriteLine("Otra forma de mostrar datos: es el año {0} y el decimal {1}", year,decimales);
             Console.WriteLine("Hola {1}, en Hotmart estamos en el año: {0}",year,nombre);
 
+            int totalSuma = Sumar(5, 10);
+            Console.WriteLine("La suma de los dos números es: " + totalSuma);
+
+            Console.WriteLine("Reutilizando la Función Sumar(): con diferentes valores");
+            int totalSuma2 = Sumar(25, 15);
+            Console.WriteLine("La suma de la segunda vez de la funcion es: " + totalSuma2);
+            int totalSuma3 = Sumar(20, 55);
+            Console.WriteLine("La suma de la tercera vez de la funcion es: " + totalSuma3);
+
             Console.ReadLine();
+        }
+
+        static int Sumar(int num1, int num2)
+        {
+            return num1 + num2;
         }
     }
 }
